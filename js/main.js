66c4fcffe957bd30a299f8cd30c40dc55d998c70
Lucas Julia <lucasfjulia@gmail.com>
2023-09-17 20:42:43 -0300

    mediaqueries

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ const carrito = document.querySelector('.carrito');
 const contenedorCarrito = document.querySelector('.carrito__lista tbody');
 const ofertas = document.querySelector('.special-offers');
 const productos = document.querySelector('.products')
-const vaciarCarritoBtn = document.querySelector('.carrito__vaciar');
-
+const vaciarCarritoBtn = document.querySelector('#carrito__vaciar');
+const productoSwiper = document.querySelector('.swiper-wrapper');
+const specialOferts = document.querySelector('.special-offers__container');
+const carouselItem = document.querySelector('.carousel-inner');
 let articulos = [];
+let productHTML = document.createElement('div');
 
 cargarEventListeners();
 
@@ -20,14 +23,67 @@ function cargarEventListeners(){
 
     document.addEventListener('DOMContentLoaded', () => {
         articulos = JSON.parse(localStorage.getItem('carrito')) || [];
-        console.log(articulos)
+       
         carritoHTML();
 
     })
+    
+    document.addEventListener('DOMContentLoaded', function () {
+    
+        
+        async function fecthProducts(url) {
+            let data = await fetch(url);
+            let response = await data.json();
+            
+         
+           
+            for (let i= 0; i < 4; i++) {
+                specialOferts.innerHTML += `
+                <article class="special-offers__card" >
+                    <div class="special-offers__card__texts">
+                    <h3>${response[i].title}</h3>
+                    <div class="special-offers__card__texts__price">
+                    <span>${response[i].price + 100}</span>
+                    <span class="price">${response[i].price}</span>
+                    </div>
+                    <a href="" class="addtocart" data-id="${response[i].id}">Agregar al carrito</a>
+                    </div>
+                    <img src="${response[i].image}" alt="">
+                </article>   
+                `
+               
+                
+            }
 
-   
+            for (let index = 14; index < 20; index++) {
+                productoSwiper.innerHTML += `
+                <div class="swiper-slide">
+                    <article class="product">
+                    <img src="${response[index].image}" class="product__img"   alt="">
+                    <div class = "product__texts">
+                    <h3 class="product__name">${response[index].title}</h3>
+                    <p class="product__info">Excelente calidad</p>
+                    <p class="product__price price">${response[index].price}</p>
+                    </div>
+                    <a href="" class="addtocart" data-id="${response[index].id}">Agregar Carrito</a>
+                    </article>
+                </div>
+               
+                `
+             
+            }
+
+            
+        }
+        fecthProducts('https://fakestoreapi.com/products');
+
+       
+    })
 
 }
+   
+
+
 
 //Funcion para agregar productos
 
@@ -36,6 +92,7 @@ function agregarProducto(e){
     if(e.target.classList.contains('addtocart')){
         const productoSeleccionado = e.target.parentElement.parentElement;
         leerDatos(productoSeleccionado);
+        
         
     }  
     
@@ -113,8 +170,11 @@ function sincronizarStorage(){
 // Elimina los cursos del carrito en el DOM
 function vaciarCarrito() {
 
+    
     while(contenedorCarrito.firstChild) {
-         contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-     }
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+    localStorage.removeItem('carrito');
+    
 }
 
--- js/script.js
+++ js/script.js
@@ -21,3 +21,30 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+swiper.on('init', () => {
+  if (window.innerWidth <= 576) {
+    swiper.params.slidesPerView = 1; // Cambiar a 1 elemento por vista
+    swiper.update(); // Actualizar el swiper con la nueva configuración
+  } else if (window.innerWidth <= 768) {
+    swiper.params.slidesPerView = 2; // Cambiar a 2 elementos por vista
+    swiper.update(); // Actualizar el swiper con la nueva configuración
+  }
+});
+
+// Escuchar el evento de cambio de tamaño de la ventana
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 576 && swiper.params.slidesPerView !== 1) {
+    swiper.params.slidesPerView = 1; // Cambiar a 1 elemento por vista
+    swiper.update(); // Actualizar el swiper con la nueva configuración
+  } else if (window.innerWidth <= 768 && swiper.params.slidesPerView !== 2) {
+    swiper.params.slidesPerView = 2; // Cambiar a 2 elementos por vista
+    swiper.update(); // Actualizar el swiper con la nueva configuración
+  } else if (window.innerWidth > 768 && swiper.params.slidesPerView !== 3) {
+    swiper.params.slidesPerView = 3; // Cambiar de vuelta a 3 elementos por vista
+    swiper.update(); // Actualizar el swiper con la nueva configuración
+  }
+});
+
+// Inicializar el swiper
+swiper.init();
+
